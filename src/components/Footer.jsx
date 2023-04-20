8064dbe0ebae16ae30cc5f6601c84074e1933a69
lesha <oookosmos.m@yandex.ru>
2023-04-20 23:57:51 +0300

    Develop

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-        <p className="footer__name">&copy;2020Mesto Russia</p>
-     </footer>
-    );
-  }
-  
-  export default Footer;
